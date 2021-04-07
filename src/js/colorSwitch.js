3fe617ac9829c0b6c0002642504332556c41f0f2
IvanBuzduha <ivan.bii33@gmail.com>
2021-04-07 19:09:09 +0300

    создает все основные файлы

---
+++ src/js/colorSwitch.js
@@ -0,0 +1,29 @@
+const colors = [
+    '#FFFFFF',
+    '#2196F3',
+    '#4CAF50',
+    '#FF9800',
+    '#009688',
+    '#795548',
+  ];
+  
+   const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+const refs = {
+  start: document.querySelector('[start]'),
+  stop: document.querySelector('[stop]'),
+};
+  let timer;
+  refs.start.addEventListener('click', () => {
+  timer = setInterval(() => {
+  document.body.style.background = colors[randomIntegerFromInterval(0, colors.length-1)];
+  }, 1000);
+    refs.stop.removeAttribute('disabled');
+    refs.start.setAttribute('disabled', true);
+  });
+  refs.stop.addEventListener("click", () => {
+  clearInterval(timer);
+   refs.start.removeAttribute('disabled');
+   refs.stop.setAttribute('disabled', true);
+  });
